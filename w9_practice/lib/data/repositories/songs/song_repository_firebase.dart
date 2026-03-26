19a171c3dde1ffc037d1273bf284215624abae72
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-03-26 09:10:14 +0700

    W9-01 – Fetch and display songs in the library screen

--- w9_practice/lib/data/repositories/songs/song_repository_firebase.dart
+++ w9_practice/lib/data/repositories/songs/song_repository_firebase.dart
@@ -7,7 +7,10 @@ import '../../dtos/song_dto.dart';
 import 'song_repository.dart';
 
 class SongRepositoryFirebase extends SongRepository {
-  final Uri songsUri = Uri.https('YOUR FIREBASE URL', '/songs.json');
+  final Uri songsUri = Uri.https(
+    'w9-practice-4a4bf-default-rtdb.firebaseio.com',
+    '/songs.json',
+  );
 
   @override
   Future<List<Song>> fetchSongs() async {
@@ -15,8 +18,17 @@ class SongRepositoryFirebase extends SongRepository {
 
     if (response.statusCode == 200) {
       // 1 - Send the retrieved list of songs
-      List<dynamic> songJson = json.decode(response.body);
-      return songJson.map((item) => SongDto.fromJson(item)).toList();
+
+      final Map<String, dynamic> songs =
+          json.decode(response.body) as Map<String, dynamic>;
+
+      return songs.entries.map((element) {
+        final String id = element.key;
+        return SongDto.fromJson({
+          'id' : id,
+          ...element.value
+        });
+      }).toList();
     } else {
       // 2- Throw expcetion if any issue
       throw Exception('Failed to load posts');
